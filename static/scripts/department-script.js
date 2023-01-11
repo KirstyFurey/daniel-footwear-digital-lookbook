abfff1a3138f526d0be1ccd9c585f9b504635fce
Daniel Footwear <danielfootwear@Daniels-MacBook-Pro.local>
2023-01-11 10:56:07 +0000

    initial com

---
+++ static/scripts/department-script.js
@@ -0,0 +1,131 @@
+// JavaScript Document
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Next/previous controls
+const plusSlides = (n) => {
+  showSlides(slideIndex += n);
+}
+
+const prev = document.getElementById("prev");
+
+prev.onclick = () => {
+	plusSlides(-1);
+}
+
+const next = document.getElementById("next");
+
+next.onclick = () => {
+	plusSlides(1);
+}
+
+
+// swipe to move image
+let touchstartX = 0;
+let touchendX = 0;
+
+const gestureZone = document.getElementById('style-container');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+		plusSlides(1);
+    }
+    
+    if (touchendX > touchstartX) {
+		plusSlides(-1);
+    }
+    
+}
+
+/*
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const gestureZone = document.getElementById('gestureZone');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX <= touchstartX) {
+        console.log('Swiped left');
+    }
+    
+    if (touchendX >= touchstartX) {
+        console.log('Swiped right');
+    }
+    
+    if (touchendY <= touchstartY) {
+        console.log('Swiped up');
+    }
+    
+    if (touchendY >= touchstartY) {
+        console.log('Swiped down');
+    }
+    
+    if (touchendY === touchstartY) {
+        console.log('Tap');
+    }
+}
+
+/*
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+*/
---
+++ static/scripts/script.js
@@ -0,0 +1,62 @@
+// JavaScript Document
+
+const btns = document.getElementsByClassName('btn'); // GET ANY ELEMENT WITH A CLASS OF BTN FOR MODAL
+const close = document.getElementsByClassName('close'); // GET ANY ELEMENT WITH CLASS OF CLOSE
+const modals = document.getElementsByClassName('modal'); // GET ANY ELEMENT WITH CLASS OF MODAL
+
+//OPEN AND CLOSE MULTIPLE MODALS
+//WORKS BY INDEXING THE MODAL AND BTN CLASSES SO CLICKING ON THE 3RD ELEMENT WITH A BTN CLASS OPENS THE 3RD ELEMENT WITH THE MODAL CLASS
+[...btns].forEach((btn, ind) => {
+    btn.onclick = () => (modals[ind].style.display = 'block');
+});
+
+[...close].forEach((close, ind) => {
+    close.onclick = () => (modals[ind].style.display = 'none');
+});
+
+
+// CLOSE MODAL ON CLICK OUTSIDE MODAL CONTENT. WORKS BY ADDING THE CLASS 'MODAL' TO THE CONTAINER ELEMENT (THAT HOLDS THE MODAL CONTENT), AND SETTING THIS TO THE FULL SIZE OF THE PAGE, SO CLICKING ANYWHERE OUTSIDE OF THE ELEMENT WITH THE MODAL CONTENT IS CLICKING ON THE ELEMENT WITH THE CLASS OF MODAL
+window.onclick = (e) => {
+    [...modals].forEach((modals) => {
+        if (e.target == modals) {
+            modals.style.display = 'none';
+        }
+    });
+};
+/*
+const createStyle = () => {
+    styles.forEach(style => {
+        const styleDiv = createElem('div', 'img-container');
+        const getStyleContainer = document.getElementById('style-container');
+        getStyleContainer.appendChild(styleDiv);
+        const styleImage = createElem('img', 'btn');
+        styleImage.classList.add('btn', 'img-style');
+        styleImage.src = style.img;
+        styleDiv.appendChild(styleImage);
+    })
+}
+
+// for creating an element
+const createElem = (tagName, className) => {
+    const element = document.createElement(tagName);
+    element.setAttribute('class', className);
+    return element;
+}
+
+window.onload = () => {
+    createStyle();
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
